40eefd31df5ea5dcf3337c0d47aff0d8fb079c44
philipnwabuwa <philipnwabuwa@gmail.com>
2024-04-08 09:00:32 +0000

    chore: added dashboard page.

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Navbar from "@/componets/common/Navbar";
+import Sidebar from "@/componets/common/Sidebar";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <body
+      className="app-default"
+      id="kt_app_body"
+      data-kt-app-sidebar-enabled="true"
+      data-kt-app-sidebar-fixed="true"
+      data-kt-app-sidebar-push-header="true"
+      data-kt-app-sidebar-push-toolbar="true"
+      data-kt-app-sidebar-push-footer="true"
+      data-kt-app-toolbar-enabled="true"
+    >
+      <div className="d-flex flex-column flex-root app-root" id="kt_app_root">
+        <div className="app-page flex-column flex-column-fluid" id="kt_app_page">
+          <Navbar />
+          <Sidebar />
+        </div>
+      </div>
+    </body>
+  );
 };
 
 export default Dashboard;
