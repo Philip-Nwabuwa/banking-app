68ea5733f56f77c646529b5028344c9ad49054c7
philipnwabuwa <philipnwabuwa@gmail.com>
2024-04-15 14:49:26 +0000

    fix: build Uncaught Error: Minified React error 1.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ThemeProvider>
-        <>{children}</>
+        <body>{children}</body>
         <Toaster richColors position="top-center" />
       </ThemeProvider>
     </html>
